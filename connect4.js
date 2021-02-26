165a7f26c60d4957a32ea48f49444c4886305eb4
Matt Allen Robinson <robinsonmattallen@gmail.com>
2021-02-26 16:36:29 -0500

    completed and fully functioning game

---
+++ connect4.js
@@ -0,0 +1,148 @@
+/** Connect Four
+ *
+ * Player 1 and 2 alternate turns. On each turn, a piece is dropped down a
+ * column until a player gets four-in-a-row (horiz, vert, or diag) or until
+ * board fills (tie)
+ */
+
+const WIDTH = 7;
+const HEIGHT = 6;
+
+let currPlayer = 1; // active player: 1 or 2
+let board = []; // array of rows, each row is array of cells  (board[y][x])
+
+/** makeBoard: create in-JS board structure:
+ *    board = array of rows, each row is array of cells  (board[y][x])
+ */
+
+function makeBoard() {
+  for (let y = 0; y < HEIGHT; y++) {
+    board.push(Array.from({ length: WIDTH }));
+  }
+}
+
+/** makeHtmlBoard: make HTML table and row of column tops. */
+
+function makeHtmlBoard() {
+  const htmlBoard = document.querySelector("#board");
+
+  // creates top row that users can click on to play pieces on the board
+  const top = document.createElement("tr");
+  top.setAttribute("id", "column-top");
+  top.addEventListener("click", handleClick);
+
+  for (let x = 0; x < WIDTH; x++) {
+    let headCell = document.createElement("td");
+    headCell.setAttribute("id", x);
+    top.append(headCell);
+  }
+  htmlBoard.append(top);
+
+  // creates rows and cells that will be filled during the course of the game
+  for (let y = 0; y < HEIGHT; y++) {
+    const row = document.createElement("tr");
+    for (let x = 0; x < WIDTH; x++) {
+      const cell = document.createElement("td");
+      cell.setAttribute("id", `${y}-${x}`);
+      row.append(cell);
+    }
+    htmlBoard.append(row);
+  }
+}
+
+/** findSpotForCol: given column x, return top empty y (null if filled) */
+
+function findSpotForCol(x) {
+  for (let y = HEIGHT - 1; y >= 0; y--) {
+    if (!board[y][x]) {
+      return y;
+    }
+  }
+  return null;
+};
+
+/** placeInTable: update DOM to place piece into HTML table of board */
+
+function placeInTable(y, x) {
+  const newPiece = document.createElement('div');
+  newPiece.classList.add('piece');
+  newPiece.classList.add(`p${currPlayer}`);
+
+  const place = document.getElementById(`${y}-${x}`);
+  place.append(newPiece);
+}
+
+/** endGame: announce game end */
+
+function endGame(msg) {
+  alert(msg);
+}
+
+/** handleClick: handle click of column top to play piece */
+
+function handleClick(evt) {
+  // get x from ID of clicked cell
+  let x = +evt.target.id;
+
+  // get next spot in column (if none, ignore click)
+  let y = findSpotForCol(x);
+  if (y === null) {
+    return;
+  }
+
+  // place piece in board and add to HTML table
+ 
+  board[y][x] = currPlayer;
+  placeInTable(y, x);
+
+  // check for win
+  if (checkForWin()) {
+    return endGame(`Player ${currPlayer} won!`);
+  };
+
+  // check for tie
+  if (board.every(row => row.every(cell => cell))) {
+    return endGame(`It's a tie! You're both losers.`)
+  };
+
+
+  // switch players
+   currPlayer = currPlayer === 1 ? 2 : 1;
+}
+
+/** checkForWin: check board cell-by-cell for "does a win start here?" */
+
+function checkForWin() {
+  function _win(cells) {
+    // Check four cells to see if they're all color of current player
+    //  - cells: list of four (y, x) cells
+    //  - returns true if all are legal coordinates & all match currPlayer
+
+    return cells.every(
+      ([y, x]) =>
+        y >= 0 &&
+        y < HEIGHT &&
+        x >= 0 &&
+        x < WIDTH &&
+        board[y][x] === currPlayer
+    );
+  }
+
+  // These 'for' loops calculate the coordinates necessary for the 4 types of victory.
+  for (let y = 0; y < HEIGHT; y++) {
+    for (let x = 0; x < WIDTH; x++) {
+      var horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
+      var vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
+      var diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
+      var diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
+
+      // This 'if' statement returns a victory for the player that achieves any of the 4 winning combos.
+      if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
+        return true;
+      }
+    }
+  }
+}
+
+makeBoard();
+makeHtmlBoard();
